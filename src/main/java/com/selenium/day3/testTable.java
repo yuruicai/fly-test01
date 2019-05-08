9888224cf5ca4f8906acf4f948b862d09f3489cc
710347184@qq.com <YRC9941028yrc>
2019-05-08 17:53:43 +0800

    第一次提交

--- src/main/java/com/selenium/day3/testTable.java
+++ src/main/java/com/selenium/day3/testTable.java
@@ -1,11 +1,18 @@
 package com.selenium.day3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class testTable {
     public static void main(String[] args){
         WebDriver driver;
         driver = new FirefoxDriver();
+        String url = "C:\\Users\\lenovo\\IdeaProjects\\Test\\src\\main\\resources\\html\\table.html";
+        driver.get(url);
+        //定位table
+        WebElement Table = driver.findElement(By.tagName("table"));
+        Table.findElement(By.tagName("tr"));
     }
 }
