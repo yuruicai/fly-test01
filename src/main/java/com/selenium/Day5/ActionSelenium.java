812b9d1f32aa95f91f5c37457c41fbc2da13a9b6
710347184@qq.com <YRC9941028yrc>
2019-05-22 17:34:36 +0800

    第一次提交

--- src/main/java/com/selenium/Day5/ActionSelenium.java
+++ src/main/java/com/selenium/Day5/ActionSelenium.java
@@ -3,8 +3,9 @@ package com.selenium.Day5;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 public class ActionSelenium {
     public WebDriver driver = null;
@@ -20,9 +21,8 @@ public class ActionSelenium {
      * **/
     public void InputBox(){
 
-        WebElement login = driver.findElement(By.id("js-signin-btn"));
+        driver.findElement(By.id("js-signin-btn")).click();
         //user.sendKeys("17816873856");
-        login.click();
 
         WebElement user = driver.findElement(By.name("email"));
         user.sendKeys("17816873855");
@@ -34,11 +34,43 @@ public class ActionSelenium {
         System.out.println(user_value);
         boolean enable = user.isEnabled();
         System.out.println(enable);
+        driver.findElement(By.name("password")).sendKeys("Summer123");
+        driver.findElement(By.className("moco-btn-red")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
+    /**
+     * 单选框
+     * */
+    public void rediobox(){
+        driver.get("https://www.imooc.com/user/setprofile");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.className("pull-right")).click();
+        WebElement sex_node = driver.findElement(By.className("moco-control-input"));
+        List<WebElement> list_sex = sex_node.findElements(By.tagName("input"));
+        for(WebElement sex:list_sex){
+            boolean flag = sex.isSelected();
+            if(flag == true){
+                continue;
+            }else {
+                sex.click();
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args){
         ActionSelenium action = new ActionSelenium();
         action.InitDriver();
         action.InputBox();
-
+        action.rediobox();
     }
 }
