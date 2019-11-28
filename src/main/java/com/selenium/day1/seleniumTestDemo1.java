31073bf6d9dd48fe0db604a8d8b5525e68fe7eb8
710347184@qq.com <YRC9941028yrc>
2019-11-28 16:39:45 +0800

    2.0

--- src/main/java/com/selenium/day1/seleniumTestDemo1.java
+++ src/main/java/com/selenium/day1/seleniumTestDemo1.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class seleniumTestDemo1 {
     public static void main(String[] args){
+        //实例化一个对象Firefox
         WebDriver driver = new FirefoxDriver();
         driver.get("http://www.baidu.com");
         System.out.println("hello!");
